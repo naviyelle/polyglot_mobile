9b2996d3dc0ac321a3383ea499ea2f138e75bb56
naviyelle <ivan21alicante@gmail.com>
2016-08-26 15:13:01 -0700

    massive changes

--- js/custom.js
+++ js/custom.js
@@ -5,23 +5,32 @@ function login(){
     var closebutton = document.getElementById("closebutton");
     var uname = document.getElementById("username").value;
     var pwd = document.getElementById("password").value;
-    
 
-    
+    //alert(sessionId);
     $$.post('http://polyglot.world/polyglot_auth/login.php', {username: uname, password: pwd}, function (data) {
        var datas = JSON.parse(data);
         var user = datas.userid;
         console.log(datas);
        
-         
+        
         if(user != undefined){
              console.log(user);
              var user_name = datas.firstname + " " + datas.lastname;
              var email_add = datas.email;
-             
+
+
+
+            localStorage.setItem("firstname", datas.firstname);
+            localStorage.setItem("lastname", datas.lastname);
+            localStorage.setItem("nickname", datas.firstname);
+            localStorage.setItem("email", datas.email);
+            localStorage.setItem("password", datas.password);
+
+                    
             closebutton.click();
             document.getElementById("user_name").innerHTML = user_name;
             document.getElementById("email_add").innerHTML = email_add;
+
         }
         
         
@@ -33,3 +42,21 @@ function login(){
     
 
 }
+$$(document).on('pageInit', '.page[data-page="setting"]', function (e) {
+
+ myApp.showPreloader();
+        setTimeout(function () {
+            myApp.hidePreloader();
+        }, 2000);
+})  
+
+$$(document).on('pageAfterAnimation', '.page[data-page="setting"]', function (e) {
+  // Do something here when page with data-page="about" attribute loaded and initialized
+  document.getElementById('fname').value = localStorage.getItem("firstname");
+  document.getElementById('lname').value = localStorage.getItem("lastname");
+  document.getElementById('nname').value = localStorage.getItem("nickname");
+  document.getElementById('email').value = localStorage.getItem("email");
+  document.getElementById('password').value = localStorage.getItem("password");
+
+  //alert("dsa");
+})  
