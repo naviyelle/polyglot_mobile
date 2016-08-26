e1c04c4062c7aa9c3b7c69337182998a749b289f
naviyelle <ivan21alicante@gmail.com>
2016-08-26 11:07:09 -0700

    custom chnge format

--- js/custom.js
+++ js/custom.js
@@ -5,46 +5,27 @@ function login(){
     var closebutton = document.getElementById("closebutton");
     var uname = document.getElementById("username").value;
     var pwd = document.getElementById("password").value;
-    // x.className += " close-login-screen";
-    //$.ajax({
-    //url: 'http://localhost/polyglot_auth/login.php',
-    //type: 'post',
-    //data: {username: uname, password: pwd},
-    //success: function(response) {
-        //console.log(response);
-         //if login is true
-         //x.className += " close-login-screen";
-    
-        //else
-        
-       //}
-   // });
+
     
     $$.post('http://polyglot.world/polyglot_auth/login.php', {username: uname, password: pwd}, function (data) {
        var datas = JSON.parse(data);
-        var user = datas.numberofuser;
+        var user = datas.userid;
         console.log(datas);
-        //alert(data);
-        //alert(datas.numberofuser);
-        //test(response);
        
         
-        if(user == "1"){
+        if(user != undefined){
              console.log(user);
+             var user_name = datas.firstname + " " + datas.lastname;
+             
             closebutton.click();
-             //x.className += " close-login-screen";
-            //x.click();
-            //user = "0";
-            //$("loginbutton").trigger("select");
-            //end();
-            //x.click;
+            document.getElementById("user_name").innerHTML = user_name;
         }
         
         
         
     },JSON);
     
-    //alert("data");
+
    
     
 
